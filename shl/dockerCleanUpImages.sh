4d9005ddb1e73c1b54060dc2b8e951bc03c6afbc
sebma <sebma@users.noreply.github.com>
2025-02-25 18:43:44 +0100

    Update dockerCleanUpImages.sh
    mawk does not understand '\s' notation.

--- shl/dockerCleanUpImages.sh
+++ shl/dockerCleanUpImages.sh
@@ -3,4 +3,4 @@
 type sudo >/dev/null 2>&1 && [ $(id -u) != 0 ] && groups | egrep -wq "sudo|adm|admin|root|wheel" && sudo="sudo" || sudo=""
 groups | egrep -w docker && docker=docker || docker="$sudo docker"
 
-$docker image ls | awk '/^<none>\s+<none>/{printf" "$3}' | \xargs -rt $docker rmi
+$docker image ls | awk '/^<none>[[:space:]]+<none>/{printf" "$3}' | \xargs -rt $docker rmi
